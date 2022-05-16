013a5e6228245991a6b952726bb02675b32f296a
Dablu Prasad <dablu.prasad@vin.net>
2022-05-16 23:47:01 +0530

    update solidity

--- contracts/FlowerCollection.sol
+++ contracts/FlowerCollection.sol
@@ -3,6 +3,7 @@ pragma solidity >=0.4.22 <0.9.0;
 
 import '@openzeppelin/contracts/token/ERC1155/ERC1155.sol';
 import '@openzeppelin/contracts/access/Ownable.sol';
+import "@openzeppelin/contracts/utils/Strings.sol";
 
 contract FlowerCollection is Ownable, ERC1155 {
     // Base URI
@@ -11,7 +12,7 @@ contract FlowerCollection is Ownable, ERC1155 {
 
     constructor()
         ERC1155(
-            'ipfs://QmW6BQqh6zRtcFtaqUNaxnDEzDtfG24dHaLp7txXoQuMhv/{id}.json'
+            'https://gateway.pinata.cloud/ipfs://QmW6BQqh6zRtcFtaqUNaxnDEzDtfG24dHaLp7txXoQuMhv/{id}.json'
         )
     {
         setName('Flower Collections');
@@ -25,11 +26,30 @@ contract FlowerCollection is Ownable, ERC1155 {
         name = _name;
     }
 
+// Batch Mint of Flower collection to create TokenId
     function mintBatch(uint256[] memory ids, uint256[] memory amounts)
         public
         onlyOwner
     {
         _mintBatch(msg.sender, ids, amounts, '');
+    }
+
+// Batch Transfer of TokenId from one one account to other
+function BatchTransferFrom(address _to,uint256[] memory ids, uint256[] memory amounts)
+        public
+        onlyOwner
+    {
+        _safeBatchTransferFrom(msg.sender, _to, ids,amounts, '');
+    }
+
+// Get URI link of any Token.
+    function uri(uint256 _tokenid) override public pure returns (string memory) {
+        return string(
+            abi.encodePacked(
+                "https://gateway.pinata.cloud/ipfs/QmW6BQqh6zRtcFtaqUNaxnDEzDtfG24dHaLp7txXoQuMhv/",
+                Strings.toString(_tokenid),".json"
+            )
+        );
     }
 
     function mint(uint256 id, uint256 amount) public onlyOwner {
